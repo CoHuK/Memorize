4ce96a0a3de7bfeb3de26d7e257956a1de7a0c82
Pavel Strongin <cohuk2002@gmail.com>
2020-05-25 19:58:32 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Pasha on 25/5/20.
+//  Copyright © 2020 MemorizeApp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello There, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
